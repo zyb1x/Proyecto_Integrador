9993632eb82a63eb1317eb211cfcfaae5fc762b3
viann <viann@DESKTOP-707CGV7>
2025-06-24 13:18:18 -0500

    vianney k we

--- src/proyectointegrador3/ProyectoIntegrador3.java
+++ src/proyectointegrador3/ProyectoIntegrador3.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyectointegrador3;
 
 /**
@@ -10,9 +7,7 @@ package proyectointegrador3;
  */
 public class ProyectoIntegrador3 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
     }
