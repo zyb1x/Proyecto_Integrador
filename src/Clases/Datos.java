75ac60cf77a96471cff64e7d01b81bb72f7e00c5
viann <viann@DESKTOP-707CGV7>
2025-06-24 13:02:28 -0500

    se agregaron clases a lo descarado juas juas prros

---
+++ src/Clases/Datos.java
@@ -0,0 +1,19 @@
+
+package Clases;
+
+public class Datos {
+   
+    private String nombre, materno, paterno;
+
+    public Datos(){
+
+    }
+    
+    public Datos(String nombre, String materno, String paterno){
+        this.nombre = nombre;
+        this.materno = materno;
+        this.paterno = paterno;
+    }
+
+    
+}
