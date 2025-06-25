df4b5f74f48fa7a7d51264210f7478abbf3ed3ee
zyb1x <dluis98@hotmail.com>
2025-06-25 08:57:16 -0500

    Se agregan getter y setter en las clases a las que le faltaban

--- src/Clases/Ventas.java
+++ src/Clases/Ventas.java
@@ -10,5 +10,23 @@ public class Ventas {
     this.idVenta = idVenta;
     this.fecha = fecha;
     }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
     
 }
