1281191ffcf2eb6262239c155b5905b6acf2001d
viann <viann@DESKTOP-707CGV7>
2025-06-29 00:01:47 -0500

    cree la clase proveedor junto con sus atributos y getter and setter

---
+++ src/Clases/Proveedor.java
@@ -0,0 +1,37 @@
+
+package Clases;
+
+public class Proveedor {
+    private String fecha;
+        private int id;
+
+      /*  public Proveedor(String fecha, int id){
+            
+        }  */
+
+    public Proveedor(String fecha, int id) {
+        this.fecha = fecha;
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+        
+        
+        
+        
+    
+}
