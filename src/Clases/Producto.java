056e4e180039580ce4f7f106daf82aad8ba80b8e
viann <viann@DESKTOP-707CGV7>
2025-06-28 23:57:46 -0500

    se le agregaron los getters and setters a producto

--- src/Clases/Producto.java
+++ src/Clases/Producto.java
@@ -1,10 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-
 public class Producto {
+    private String nombre;
+    private int idProducts, stock;
+    private String tamannio;
+    private float precio;
+
+    public Producto(String nombre, int idProducts, int stock, String tamannio, float precio) {
+        this.nombre = nombre;
+        this.idProducts = idProducts;
+        this.stock = stock;
+        this.tamannio = tamannio;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdProducts() {
+        return idProducts;
+    }
+
+    public void setIdProducts(int idProducts) {
+        this.idProducts = idProducts;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getTamannio() {
+        return tamannio;
+    }
+
+    public void setTamannio(String tamannio) {
+        this.tamannio = tamannio;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
+    
     
 }
