c2cb7ca45738827d1501888690a398aa6874e269
Pablo <Pablo@172.16.100.94>
2025-07-18 09:29:35 -0500

    revision de parte de milca aun no terminado recuperacion contraseña

--- src/Ventanas/login.java
+++ src/Ventanas/login.java
@@ -561,4 +561,8 @@ public class login extends javax.swing.JInternalFrame {
     private javax.swing.JTextField jTextField3;
     private javax.swing.JPasswordField pwd;
     // End of variables declaration//GEN-END:variables
+
+    void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
